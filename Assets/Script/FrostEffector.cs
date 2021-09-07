ef114ee80b30ac1303110a861d5c6e0966af07e6
Sute1a <yuki.t.415415@gmail.com>
2021-09-07 21:55:51 +0900

    手順２１

---
+++ Assets/Script/FrostEffector.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrostEffector : MonoBehaviour
+{
+    private float duration = 0.3f;
+
+    private bool isApplyEffected;
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Player" && isApplyEffected == false)
+        {
+            isApplyEffected = true;
+
+            Camera.main.gameObject.GetComponent<FrostEffectController>().
+                UpdateFrostAmount(duration);
+
+            Destroy(gameObject, 0.5f);
+        }   
+    }
+    
+}
